4d1ee0065407c2804f3109617e68359092e0dbd0
nyorf <me@nyorf.com>
2020-12-27 20:44:49 +0700

    6.4 complete

---
+++ block6/6.4/task1.py
@@ -0,0 +1,8 @@
+count = 10
+while count <= 10:
+    if count >= 0:
+        print(count)
+        count -= 1
+    else:
+        print('Время вышло!')
+        break
---
+++ block6/6.4/task2.py
@@ -0,0 +1,6 @@
+while True:
+    app_status = int(input('Продолжаем работать? (1 - да, 0 - нет) '))
+    if app_status == 0:
+        print('Приложение закрывается...')
+        print('Работа завершена!')
+        break
