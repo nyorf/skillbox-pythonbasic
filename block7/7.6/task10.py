e1f78b855a7f9546c21660616b647aa5daf276b1
nyorf <me@nyorf.com>
2021-01-07 11:46:22 +0700

    7.6 task10 fix

--- block7/7.6/task10.py
+++ block7/7.6/task10.py
@@ -3,10 +3,13 @@
 # Найдите ее, зная номера оставшихся карточек.
 # Вводится число N, далее еще N − 1 чисел: номера оставшихся карточек (различные числа от 1 до N).
 # Программа должна вывести номер потерянной карточки.
+inputCardSumm = 0
+cardSumm = 0
 lastCard = int(input('Введите номер последней карточки: '))
-for card in range(1, lastCard):
-    cardcheck = int(input('Введите номер карточки: '))
-    if cardcheck != card:
-        missedCard = card
-        break
+for card in range(1, lastCard + 1):
+    cardSumm += card
+for card in range(lastCard - 1):
+    remainingCard = int(input('Введите номер карточки: '))
+    inputCardSumm += remainingCard
+missedCard = cardSumm - inputCardSumm
 print('Потеряли карточку под номером', missedCard)
