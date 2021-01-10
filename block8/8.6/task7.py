930fdeaa4ed805eaa1b5527936e5144c16b2c7f0
Dmitry Dolgov <me@nyorf.com>
2021-01-10 16:40:57 +0700

    8.6 hw push

---
+++ block8/8.6/task7.py
@@ -0,0 +1,16 @@
+# Задача 7. Стипендия
+# Ежемесячная стипендия студента составляет educational_grant руб., а расходы на проживание превышают стипендию и
+# составляют expenses руб. в месяц. Рост цен ежемесячно увеличивает расходы на 3%, кроме первого месяца.
+# Составьте программу расчета суммы денег, которую необходимо получить у родителей один раз в начале обучения,
+# чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
+educational_grant = int(input('Какая у вас стипендия? '))
+expenses = int(input('А какие у вас расходы? '))
+totalGrant = 0
+totalExpenses = 0
+for month in range(2, 11):
+    counter += 1
+    totalExpenses += expenses
+    totalGrant += educational_grant
+    expenses = expenses + (expenses * 0.03)
+remainingMoney = totalExpenses - totalGrant
+print('У родителей нужно попросить', remainingMoney // 1 + 1, 'рублей.')
---
+++ block8/8.6/task9.py
@@ -0,0 +1,14 @@
+# Задача 9. Выражение
+# Дано число x. Напишите программу для вычисления следующего выражения (см. в задании)
+x = int(input('Введите число X: '))
+totalNumerator = 1
+totalDenominator = 1
+for number in range(1, 65):
+    numNumber = number * 2 - 1
+    denomNumber = 
+    numerator = x - number * 2 + 1 #
+    totalNumerator *= numerator #
+    denominator = x - number * 2 #
+    totalDenominator *= denominator #
+    print(numNumber)
+
