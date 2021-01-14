b9514e788d86221f977ed1d3cce3def886f832d9
Dmitry Dolgov <me@nyorf.com>
2021-01-14 21:57:59 +0700

    9.4 complete

--- block9/9.4/task2.py
+++ block9/9.4/task2.py
@@ -1,8 +1,9 @@
 number = int(input('Введите первое число: '))
 step = int(input('Введите шаг: '))
 summ = 0
+print('\nIP-address:', end=' ')
 for add in range(3):
     print(number, end='.')
     summ += number
     number += step
-print('\nIP-address:', summ)
+print(summ)
---
+++ block9/9.4/task3.py
@@ -0,0 +1,4 @@
+num = int(input('Введите число: '))
+print('-=-', end = ' ')
+for count in range(10, num + 1, 10):
+    print(count, end = ' -=- ')
