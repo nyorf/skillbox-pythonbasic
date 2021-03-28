7b462b665bed7f716023bf8f7bfe2d3e9e67e6ab
Dmitry Dolgov <me@nyorf.com>
2021-03-28 21:07:53 +0700

    task2 func updated

--- block13/13.6/task2.py
+++ block13/13.6/task2.py
@@ -4,10 +4,7 @@ def maxnum(check1, check2):
 def threenumcheck(num1, num2, num3):
     firstcheck = maxnum(num1, num2)
     secondcheck = maxnum(num2, num3)
-    if firstcheck < secondcheck:
-        print('Наибольшее число:', secondcheck)
-    else:
-        print('Наибольшее число:', firstcheck)
+    return maxnum(firstcheck, secondcheck)
 
 num1 = int(input('Введите первое число: '))
 num2 = int(input('Введите второе число: '))
